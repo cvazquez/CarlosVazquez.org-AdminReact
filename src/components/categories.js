7f490498e9b4794cfa2bfdefe05c8aa0284af88f
Carlos Vazquez <cvazquez1976@gmail.com>
2020-06-26 21:59:29 -0400

    Add add category functionality

--- src/components/categories.js
+++ src/components/categories.js
@@ -9,13 +9,18 @@ export default class Categories extends React.Component {
 			isLoaded			: false,
 			categories			: [],
 			categoriesByName	: {},
-			categoriesById		: {}
+			categoriesById		: {},
+			newCategory			: "",
+			savedCategory		: null,
+			newCategorySaveStatus : null
 		};
 
 		this.categoriesById = []; // use for before and after state comparisons when saving
 
-		this.handleTextUpdate = this.handleTextUpdate.bind(this);
-		this.handleCategoryBlur = this.handleCategoryBlur.bind(this);
+		this.handleTextUpdate		= this.handleTextUpdate.bind(this);
+		this.handleCategoryBlur		= this.handleCategoryBlur.bind(this);
+		this.handleNewCategory		= this.handleNewCategory.bind(this);
+		this.handleNewCategorySubmit	= this.handleNewCategorySubmit.bind(this);
 	}
 
 	getCategories() {
@@ -67,6 +72,7 @@ export default class Categories extends React.Component {
 	}
 
 	handleCategoryBlur(e) {
+		// A blanked out category will deactivate it
 		const	id		= e.currentTarget.dataset.id,
 				name	= e.target.value;
 
@@ -115,8 +121,54 @@ export default class Categories extends React.Component {
 			});
 	}
 
+	handleNewCategory(e) {
+		this.setState({
+			newCategory : e.target.value
+		});
+	}
+
+	handleNewCategorySubmit(e) {
+		e.preventDefault();
+
+		fetch(`${process.env.REACT_APP_API_URL}/blog/api/admin/addCategory`, {
+			method	: 'POST',
+			body	: JSON.stringify({newCategory : this.state.newCategory}),
+			headers	: {	'Content-Type': 'application/json'}
+		})
+		.then(res => res.json())
+		.then( result => {
+			if(result.addedCategory.affectedRows && result.addedCategory.affectedRows > 0) {
+
+				this.setState({
+					newCategorySaveStatus	 : <span className='blink'>SAVED "{this.state.newCategory}"!</span>,
+					savedCategory			: this.state.newCategory,
+					newCategory				: ""
+				});
+
+				this.getCategories();
+
+				setTimeout(() => {
+					this.setState({
+						newCategorySaveStatus : null
+					});
+				}, 5000);
+
+			} else {
+				this.setState({
+					newCategorySaveStatus : <span className='blink'>FAILED SAVING! {result.addedCategory.message}</span>
+				})
+			}
+		},
+		error => {
+			this.setState({
+				error,
+				newCategorySaveStatus : <span className='blink'>FAILED SAVING!</span>
+			})
+		});
+	}
+
 	render() {
-		const {error, isLoaded, categoriesByName, categoriesById} = this.state;
+		const {error, isLoaded, categoriesByName, categoriesById, newCategory, newCategorySaveStatus} = this.state;
 
 		if (error) {
 			return <div>Error: {error.message}</div>;
@@ -124,18 +176,35 @@ export default class Categories extends React.Component {
 			return <div>Loading...</div>;
 		  } else {
 			return (
-				<div className="categories">
-					{Object.keys(categoriesByName).map((key) => (
-						<div key		= {key}>
+				<div>
+					<div>
+
+					</div>
+					<div className="categories">
+						<form action="POST" onSubmit={this.handleNewCategorySubmit}>
 							<input	className	= "category"
 									type		= "text"
-									value		= {categoriesById[categoriesByName[key]].name}
-									data-id		= {categoriesByName[key]}
-									onChange	= {this.handleTextUpdate}
-									onBlur		= {this.handleCategoryBlur} />
-							<span className="category-save-status">{categoriesById[categoriesByName[key]].saveStatus}</span>
-						</div>
-					))}
+									value		= {newCategory}
+									onChange	= {this.handleNewCategory}
+									onBlur		= {this.handleNewCategoryBlur}
+									placeholder	= "Add a New Category" />
+
+							<button type="button" onClick={this.handleNewCategorySubmit}>Add</button>
+							<span className="new-category-save-status">{newCategorySaveStatus}</span>
+						</form>
+
+						{Object.keys(categoriesByName).map((key) => (
+							<div key		= {key}>
+								<input	className	=  {this.state.savedCategory === categoriesById[categoriesByName[key]].name ? "category saved" : "category"}
+										type		= "text"
+										value		= {categoriesById[categoriesByName[key]].name}
+										data-id		= {categoriesByName[key]}
+										onChange	= {this.handleTextUpdate}
+										onBlur		= {this.handleCategoryBlur} />
+								<span className="category-save-status">{categoriesById[categoriesByName[key]].saveStatus}</span>
+							</div>
+						))}
+					</div>
 				</div>
 			)
 		}
